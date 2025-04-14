beda0cf1fe868f0facfb5e5e24e5e60327ad1d07
faizal <faizal@MacBook-Pro-76.local>
2025-04-14 17:40:32 +0530

    admin panel changes

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask,redirect,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+
+def index():
+    return render_template('index.html')
+
+
+@app.route('/admin')
+
+def admin():
+    return render_template('admin.html')
+
+@app.route('/incidents')
+
+def incidents():
+    return render_template("incidents.html")
+
+@app.route('/report-incident/')
+
+def report_incident():
+    return render_template('report_incident.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)  
