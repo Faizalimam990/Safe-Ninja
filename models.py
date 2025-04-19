c7fd5c049dda28bf0801b47d9ebde212f440a38e
faizal <faizal@MacBook-Pro-76.local>
2025-04-19 23:39:39 +0530

    Business detals added

--- models.py
+++ models.py
@@ -1,1 +1,19 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Float, DateTime, LargeBinary
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.sql import func
+
+Base = declarative_base()
+
+
+
+
+class Business(Base):
+    __tablename__='Business'
+    name=Column(String(255),nullable=False)
+    
+
+class Incidents(Base):
+    __tablename__='incidents'
+    id=Column(Integer,primary_key=True)
+    Title=Column(String(255),nullable=False)
+    Description=Column(String(500),nullable=False)
